aa0f1cd318099ac535e1e3c0c976234922233944
sjq <sjq@itcast.cn>
2019-07-12 21:22:51 +0800

    再次修改

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -7,6 +7,7 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        //hello
 
     }
 
